224999515281c61a5fb3d05be99a3d1f39f3245a
carlodev <carlobrunellibs@gmail.com>
2023-04-06 16:58:01 +0200

    plate 3d

--- src/Plate/BoundaryConditions.jl
+++ src/Plate/BoundaryConditions.jl
@@ -1,22 +1,55 @@
 function bc_plate(params)
 
-    u_in = params[:u_in]
+    @unpack u_in, D = params
     
     
-    u_top(x, t) = VectorValue(u_in, 0)
+    u_top(x, t) = (D == 2) ? VectorValue(u_in, 0.0) : VectorValue(u_in, 0, 0.0)
     u_top(t::Real) = x -> u_top(x, t)
 
-    u_wall(x, t) = (x[1]<0.5) ? u_top(x, t) : VectorValue(0,0)
+    u_wall(x, t) = (x[1]<0.5) ? u_top(x, t) : (D == 2) ? VectorValue(0.0, 0.0) : VectorValue(0.0, 0, 0.0)
     u_wall(t::Real) = x -> u_wall(x, t)
 
 
 
+    if D == 2
+        #Points
+        #tag_1: bottom left
+        #tag_2: bottom right
+        #tag_3: top right
+        #tag_4: top left
+        #Lines
+        #tag_5: horizonal bottom
+        #tag_6: horizonal top
+        #tag_7: veritcal left
+        #tag_8: veritcal right
+        u_diri_tags=["tag_1","tag_3", "tag_7", "tag_6", "tag_5", "tag_2"]
+        u_diri_values = [u_top, u_top, u_top, u_top, u_wall, u_wall]
+        p_diri_tags= ["tag_8", "tag_4"]
+        p_diri_values = [0.0, 0.0]
+    elseif D == 3
+
+        top = ["tag_20","tag_24"] # face
+
+        bottom = ["tag_18", "tag_23"] # face
     
-    u_diri_tags=["tag_1","tag_3", "tag_7", "tag_6", "tag_5", "tag_2"]
-    u_diri_values = [u_top, u_top, u_top, u_top, u_wall, u_wall]
-    p_diri_tags= ["tag_8", "tag_4"]
-    p_diri_values = [0.0, 0.0]
-   
+        inlet = ["tag_17", "tag_19", "tag_25"] #bottom, top, face
+        outlet = ["tag_26"] #face
+        
+
+      
+        u_diri_tags = append!(top, bottom, inlet)
+
+        u_diri_values = [u_top, u_top, 
+                        u_wall, u_wall,
+                        u_top, u_top, u_top]
+
+        p_diri_tags = outlet
+      
+        p_diri_values = [0.0]
+        @assert length(u_diri_values) == length(u_diri_tags)
+        @assert length(p_diri_values) == length(p_diri_tags)
+        
+        end
 
     return u_diri_tags, u_diri_values, p_diri_tags, p_diri_values, u_top
 
--- src/Plate/Plate.jl
+++ src/Plate/Plate.jl
@@ -3,11 +3,19 @@ include("BoundaryConditions.jl")
 
 
 function run_plate(params)
-    dim_not_supported(params[:D], params[:case])
+    # dim_not_supported(params[:D], params[:case])
     function stretching(x::Point)
-        m = zeros(length(x))
+      D = length(x)
+      m = zeros(D)
+      if D == 2
+       
         m[1] = x[1]
         m[2] = x[2].^2
+      elseif D == 3
+        m[1] = x[1]
+        m[2] = x[2].^2
+        m[3] = x[3]
+      end
         Point(m)
       end
       
@@ -16,25 +24,41 @@ function run_plate(params)
     params[:u_in] = 10.0
 
     Lx = 3
-    Ly = 1
+    Ly = 0.5
+    Lz = 0.2
+
+    @unpack N,D = params
+    if D == 2
+      Nx, Ny = N
+      domain = (0, Lx, 0, Ly)
+      partition = (Nx, Ny)
+      periodic_tuple = (false, false)
+    elseif D == 3
+      Nx, Ny, Nz = N
+      domain = (0, Lx, 0, Ly, -Lz/2, Lz/2)
+      partition = (Nx, Ny, Nz)
+      periodic_tuple = (false, false,true)
+    end
+    
+    model = CartesianDiscreteModel(params[:parts], domain, partition,map=stretching, isperiodic = periodic_tuple)
+
+
     Nx = 250
     Ny = 400
     
 
-    domain = (0, Lx, 0, Ly)
-    partition = (Nx, Ny)
-    model = CartesianDiscreteModel(params[:parts], domain, partition,map=stretching)
+ 
     
     printmodel(params, model)
     hf_gen!(params)
 
-    u_diri_tags, u_diri_values, p_diri_tags, p_diri_values, u0 = bc_plate(model, params[:u_in]) #u_top in bc_liddriven taken as initial velocity
+    u_diri_tags, u_diri_values, p_diri_tags, p_diri_values, u0 = bc_plate(params) #u_top in bc_liddriven taken as initial velocity
     merge!(params, Dict(:u0 => u0, :model => model))
 
 
     
     
-    U,P,Y,X = creation_fe_spaces(params, u_diri_tags, u_diri_values, p_diri_tags, p_diri_values)
+    V, U, P, Q, Y, X = creation_fe_spaces(params, u_diri_tags, u_diri_values, p_diri_tags, p_diri_values)
     println("spaces created")
 
     degree = 4*params[:order]
