c19cc4730cc0d0acbf8ecffe5412eda3c0e81b0d
carlodev <carlobrunellibs@gmail.com>
2023-05-24 10:36:53 +0200

    change turbulent settings

--- run/Turbulence_Settings.jl
+++ run/Turbulence_Settings.jl
@@ -4,10 +4,10 @@ function turbulence_box()
     Δy = 0.05 / 20
     Δz = 0.2 / 10
     σ = 2 * maximum([Δy, Δz])
-    σ = 0.005 #eddy dimensions, the same in all the directions
-    y = 0:0.01:0.07
-    # z = -0.5:Δz:0.5
-    z = -0.1:0.1:0.1
+    σ = 0.02 #eddy dimensions, the same in all the directions
+    y = -3:0.1:3
+    z = -0.1:Δz:0.1
+    # z = -1:0.1:1
     
     Vboxinfo = VirtualBox(collect(y), collect(z), σ)
 
